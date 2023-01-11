05d3d0a533679095f5bcdc7643267a62b953a6e3
200508880 <200508880@student.georgianc.on.ca>
2023-01-11 11:20:59 -0500

    Initial commit

---
+++ bash/helloworld.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+helloArray=("H" "e" "l" "l" "o" " " "w" "o" "r" "l" "d")
+sleepLength=0.05
+
+
+while true; do
+  workingString=""
+  for helloLetter in "${helloArray[@]}"; do
+    workingString+=${helloLetter}
+    echo "${workingString}"
+    sleep ${sleepLength}
+  done
+  while [ ${#workingString} -gt 0 ]; do
+    newLength=$((${#workingString} - 1))
+    if [ ${newLength} -gt 0 ]; then
+      workingString=$(echo "${workingString}" | cut -c 1-${newLength})
+      echo "${workingString}"
+    else
+      echo
+      break
+    fi
+    sleep ${sleepLength}
+  done
+done
