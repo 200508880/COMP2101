2e4654cbe1b7846fc6ff5a7318ba240f6a004571
200508880 <200508880@student.georgianc.on.ca>
2023-01-15 18:43:05 -0500

    Added comments, tested execution

--- bash/helloworld.ps1
+++ bash/helloworld.ps1
@@ -1,1 +1,20 @@
-Write-Host 'Hello, World!'
+# Do Powershell comments work the same as Bash?
+# Let's find out!
+# (Turns out yes)
+# Again, do this forever
+While (1) {
+# Let's do this a little differently...
+# The goal is to change the space at the start...
+# so we still get the wave pattern but with motion instead
+  $workingString = ''
+  For($i=0; $i -le 6; $i++) {
+# Thank you Wayne for teaching me about string multiplication
+    Write-Host (" " *  $i) 'Hello World'
+# The sleep duration seemed to work fine in the Bash script
+    Start-Sleep 0.05
+  }
+  For($i=6; $i -ge 0; $i--) {
+   Write-Host (" " * $i) 'Hello World'
+   Start-Sleep 0.05
+  }
+}
--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 helloArray=("H" "e" "l" "l" "o" "_" "w" "o" "r" "l" "d")
+# Why did I make this an array? This definitely didn't need to be an array.
+
+# Set time to sleep.
+# This is used in a few different places and I got 
+# bored of changing it everywhere when feeling it out.
 sleepLength=0.05
 
-
+# Loop forever (until interrupted anyway)
 while true; do
+
+# Start fresh each loop:
   workingString=""
+
+# First half of the loop, iterate through the array...
   for helloLetter in "${helloArray[@]}"; do
+# ...and add the first letter to our working string...
     workingString+=${helloLetter}
+# ...print it to the screen...
     echo "${workingString}"
+# ...and wait.
     sleep ${sleepLength}
   done
+
+# Now do the reverse. Take letters off the end until
+# there's nothing left.
   while [ ${#workingString} -gt 0 ]; do
     newLength=$((${#workingString} - 1))
     if [ ${newLength} -gt 0 ]; then
       workingString=$(echo "${workingString}" | cut -c 1-${newLength})
       echo "${workingString}"
     else
+# This is a band-aid fix because it was getting stuck
+# printing H forever.
+# The proper fix would be to change the condition on the
+# while loop, probably.
       echo
+# This break will bring us back up to the top where we will
+# build the string up from zero again.
       break
     fi
     sleep ${sleepLength}
   done
 done
 
+# An artifact from a simpler time: the unreachable exit.
 exit
