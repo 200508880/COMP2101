5753c22f87f0eba3f1c3845a80f7fd5a65cb6498
200508880 <200508880@student.georgianc.on.ca>
2023-01-25 09:53:21 -0500

    Cleaned up sysinfo.ps1, made sysinfo.sh aware of other interface names, marked helloworlds as executable

--- bash/lab1/sysinfo.ps1
+++ bash/lab1/sysinfo.ps1
@@ -1,16 +1,14 @@
 #!/bin/pwsh
-# Surprisingly, /bin/bash is there. Symlink that points to /opt.
+# Surprisingly, /bin/pwsh is there. Symlink that points to /opt.
 Write-Host 'Hostname:'
 # Can't get rid of the empty space the header created
 Get-Host | Select Name | Format-Table -hidetableheaders
 
 Write-Host 'Host information:'
+# Explicitly specify Format-List to prevent out-of-order output
+Get-Host | Format-List
 
-Get-Host
-
-# These Write-Hosts are coming up out of order
 Write-Host 'Root filesystem status:'
-
 Get-PSDrive | Format-Table
 
 # ipconfig and Get-NetIPAddress do not exist
--- bash/lab1/sysinfo.sh
+++ bash/lab1/sysinfo.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+# This is so much easier in Bash
 echo FQDN: $(hostname)
 
+# Add a blank space
 echo
 echo Host information:
 hostnamectl
 
 echo
-echo IPv4 Address: $(ip addr show dev enp1s0 | grep -i "inet " | awk '{ print $2 }')
-echo IPv6 Address: $(ip addr show dev enp1s0 | grep -i "inet6" | awk '{ print $2 }')
+# Now is more environment-agnostic, finds non-loopback interfaces with grep's -v
+# Could be more foolproof by only fetching information on connected links
+# Should ensure that network-manager is installed
+# Grepping for inet with a trailing whitespace excludes ipv6 and ensures a single line
+# (at least in this environment)
+iface=$(nmcli --terse --field DEVICE device status | grep -v 'lo')
+echo IPv4 Address: $(ip addr show dev "${iface}" | grep -i "inet " | awk '{ print $2 }')
+# Repeat for ipv6
+echo IPv6 Address: $(ip addr show dev "${iface}" | grep -i "inet6" | awk '{ print $2 }')
 
 echo
 echo Root filesystem status:
+# Use the built-in df tool with -h for pretty size readings
 df -h
