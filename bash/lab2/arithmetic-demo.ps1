e568df90e5d3322ec30b929dbbbd2f03d13ab0d5
200508880 <200508880@student.georgianc.on.ca>
2023-01-28 19:25:54 -0500

    Fixed arithmetic-demo.ps1 after tests demonstrated that it was storing ints as strings.

--- bash/lab2/arithmetic-demo.ps1
+++ bash/lab2/arithmetic-demo.ps1
@@ -6,14 +6,16 @@
 # Taks 2: Only show: the sum and the product, each with a label
 
 # Same as in bash, prompt for three numbers, save them to variables
-$firstnum = Read-Host "Enter a number: "
-$secondnum = Read-Host "Enter another number: "
-$thirdnum = Read-Host "Enter a third number: "
+[int]$firstnum = Read-Host "Enter a number: "
+[int]$secondnum = Read-Host "Enter another number: "
+[int]$thirdnum = Read-Host "Enter a third number: "
 
 # Do the math
+# Oh shoot, these are typed as strings, aren't they
+# Survey says yes. Assign types at Read-Host stage.
 $sum = $firstnum + $secondnum + $thirdnum
 $product = $firstnum * $secondnum * $thirdnum
 
 # Surprisingly, Write-Host respects tabs. I expected this to be messier.
 Write-Host "Sum		Product"
-Write-Host "${firstNum}		${secondNum}"
+Write-Host "${sum}		${product}"
