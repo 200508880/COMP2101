9a2f1714198e93eb0a7c72cdb7321dedcb9dfe27
200508880 <200508880@student.georgianc.on.ca>
2023-01-28 20:32:41 -0500

    Finished sysinfo.sh. Not looking forward to wrestling with Powershell.

--- bash/lab2/sysinfo.sh
+++ bash/lab2/sysinfo.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 # Display system information in a pretty format.
+
+# Looks like I'll largely be duplicating what I did last week.
+
+hostname=$(hostname -s)
+FQDN=$(hostname)
+# Let's do both these things in one line to change things up:
+ip=$(ip addr show dev "$(nmcli --terse --field DEVICE device status | grep -v 'lo')" | grep -i "inet " | awk '{ print $2 }')
+# Today I learned about Perl-Compatible Regular Expressions.
+osver=$(hostnamectl | grep -oP 'Operating System: \K.*')
+fsfree=$(df -h / | tail -n +2 | awk '{ print $4 }')
+
+# Yes, shellcheck, I did mean cat instead of echo
+cat <<EOF
+
+Report for ${hostname}
+===================
+FQDN: ${FQDN}
+Operating System name and version: ${osver}
+IP Address: ${ip}
+Root Filesystem Free Space: ${fsfree}
+===================
+
+EOF
