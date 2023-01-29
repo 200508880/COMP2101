234f11f16fa37d55953d3fd9c9c9b81251b6116c
200508880 <200508880@student.georgianc.on.ca>
2023-01-28 19:15:04 -0500

    Initialized arithmetic-demo.ps1 and started (maybe completed) arithmetic-demo.sh.

---
+++ bash/lab2/arithmetic-demo.ps1
@@ -0,0 +1,6 @@
+#!/bin/pwsh
+#
+# This script replicates arithmeticdemo.sh but in powershell.
+
+# Task 1: Get 3 numbers from user input instead of assigning them
+# Taks 2: Only show: the sum and the product, each with a label
--- bash/lab2/arithmetic-demo.sh
+++ bash/lab2/arithmetic-demo.sh
@@ -2,19 +2,27 @@
 #
 # this script demonstrates doing arithmetic
 
-# Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
+# Task 1: Remove the assignments of numbers to the first and second
+# number variables. Use one or more read commands to get 3 numbers
+# from the user.
 # Task 2: Change the output to only show:
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
-
-firstnum=5
-secondnum=2
-sum=$((firstnum + secondnum))
-dividend=$((firstnum / secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+echo "Enter a number"
+read firstnum
+echo "Enter another number"
+read secondnum
+echo "Enter a third number"
+read thirdnum
+sum=$((firstnum + secondnum + thirdnum))
+product=$((firstnum *  secondnum * thirdnum))
+#fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
 
 cat <<EOF
-$firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum is $dividend
-  - More precisely, it is $fpdividend
+Sum		Product
+$sum		$product
 EOF
+#$firstnum plus $secondnum is $sum
+#$firstnum divided by $secondnum is $dividend
+#  - More precisely, it is $fpdividend
+#EOF
