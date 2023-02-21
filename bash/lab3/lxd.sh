6558dd4954bba3fb3c98a2388f70f09d01be7623
200508880 <200508880@student.georgianc.on.ca>
2023-02-21 13:42:00 -0500

    I was a fool to trust Canonical. Committing this in the hopes that this time, I will learn. I should have predicted this, but Snap and Juju actually make things much MORE complicated.

--- bash/lab3/lxd.sh
+++ bash/lab3/lxd.sh
@@ -12,14 +12,28 @@ echo "Deploying a webserver container."
 #ip=$(ip addr show dev "${iface}" | grep -i "inet " | awk '{ print $2 }')
 
 apt-cache policy curl | grep Installed | grep none > /dev/null
-[ $? ] && echo "Curl not installed. Installing Curl now." && sudo apt install curl
+[ $? ] && echo "Curl not installed. Installing Curl now." && sudo apt install curl && echo
 
 snap list | grep "^juju" > /dev/null
-[ $? ] && echo "Juju not installed. Installing Juju now." && sudo snap install juju --classic
-juju bootstrap localhost COMP2101-S22 --wait
-juju deploy apache2 --wait
+[ $? ] && echo "Juju not installed. Installing Juju now." && sudo snap install juju --classic && echo
 
+echo "Initializing Juju Controller..."
+juju bootstrap localhost COMP2101-S22
+
+echo
+echo "Installing Apache2 container..."
+juju deploy apache2
+echo "Done."
+
+echo
+echo "Checking Juju status..."
 juju status
+
+exit
+
+#while true; do
+#  apacheStatus=$(juju status apache2 | grep "^0" | awk 'NR==5{ print $2 }')
+#  [ $apacheStatus -eq "ready" ] && echo "
 
 iface=$(juju ssh 0 bash -e "ip route | head -n 1 | awk '{ print $5 }")
 ip=$(juju ssh 0 bash -e "ip addr show dev ${iface} | grep -i 'inet ' | awk '{ print $2 }')")
