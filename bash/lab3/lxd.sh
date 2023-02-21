59d4d2b77abc4b814d5943fc91d2022abc4db5c0
200508880 <200508880@student.georgianc.on.ca>
2023-02-21 14:41:36 -0500

    Successful when walking through. Pushing to save script across VM snapshot reload.

--- bash/lab3/lxd.sh
+++ bash/lab3/lxd.sh
@@ -33,16 +33,33 @@ echo
 echo "Installing Apache2 container..."
 lxc launch ubuntu:20.04 COMP2021-W23
 lxc exec COMP2021-W23 -- apt update
-lxc exec COMP2021-W23 -- apt install apache2
+lxc exec COMP2021-W23 -- apt install -y apache2
 echo "Done."
 
 echo
 echo "Gathering information..."
+# Some combination of lxc exec and awk is causing issues, process here
+# There's probably a more proper way to do it but my brain is fried so here's this mess:
 
-iface=$(lxc exec COMP2021-W23 bash -e "ip route | head -n 1 | awk '{ print $5 }")
-ip=$(lxc exec COMP2021-W23 bash -e "ip addr show dev ${iface} | grep -i 'inet ' | awk '{ print $2 }')")
+iproute=$(lxc exec COMP2021-W23 -- ip route | head -n 1)
+iface=$(echo $iproute | awk '{ print $5 }')
+echo "Container network interface name is ${iface}."
+ipRaw=$(lxc exec COMP2021-W23 --env iface=${iface} -- bash -c "ip addr show dev ${iface} | grep -i 'inet '")
+ip=$(echo $ipRaw | awk '{ print $2 }')
+# Trim the subnet prefix
+ip=${ip:0:-3}
+echo "Container IP is ${ip}."
 
-grep "${ip}" /etc/hosts | grep "COMP2101-W23"
+echo "Entering COMP2101-W23 into VM's hosts file..."
+grep "${ip}" /etc/hosts | grep "COMP2101-W23" > /dev/null
 if [ $? ]; then echo "${ip} COMP2101-W23" | sudo tee -a /etc/hosts; fi
+echo "Done."
 
-curl http://COMP2101-W23
+echo
+echo "Testing webserver..."
+curl http://COMP2101-W23 -o /tmp/curlout
+curlExitCode=$?
+head /tmp/curlout
+
+echo
+[ $curlExitCode ] && "Webserver setup successful. Consider exposing it to your local network." || echo "Webserver setup unsuccessful. It worked when I tried it!"
