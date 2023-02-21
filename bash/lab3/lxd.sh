5f7e183191ed2fd5db9d06de94d0e5e79579138f
200508880 <200508880@student.georgianc.on.ca>
2023-02-21 14:00:55 -0500

    Converted to 'pure' lxd, untested

--- bash/lab3/lxd.sh
+++ bash/lab3/lxd.sh
@@ -5,7 +5,10 @@
 # I'd like to show off something I've been learning and using at work
 #
 # Since lxd is only accessible as a Snap in 22.04 anyway, I'll be using Canonical's Snap management tools
-
+#
+# The experiment was a failure.
+#
+# Reverting back to lxd itself (which is still only available on 22.04 as a Snap)
 echo "Deploying a webserver container."
 
 #iface=$(ip route | head -n 1 | awk '{ print $5 }')
@@ -14,31 +17,32 @@ echo "Deploying a webserver container."
 apt-cache policy curl | grep Installed | grep none > /dev/null
 [ $? ] && echo "Curl not installed. Installing Curl now." && sudo apt install curl && echo
 
-snap list | grep "^juju" > /dev/null
-[ $? ] && echo "Juju not installed. Installing Juju now." && sudo snap install juju --classic && echo
+# Nope, not falling for THAT again.
+#snap list | grep "^juju" > /dev/null
+#[ $? ] && echo "Juju not installed. Installing Juju now." && sudo snap install juju --classic && echo
 
-echo "Initializing Juju Controller..."
-juju bootstrap localhost COMP2101-S22
+snap list | grep "^lxd" > /dev/null
+[ $? ] && echo "lxd not installed. Installing lxd now." && sudo snap install lxd --classic && echo
+
+echo "Initializing lxd container..."
+
+ip link show | grep "lxdbr0" > /dev/null
+[ $? ] && echo "Setting up network bridge..." && lxd init --auto
 
 echo
 echo "Installing Apache2 container..."
-juju deploy apache2
+lxc launch ubuntu:20.04 COMP2021-W23
+lxc exec COMP2021-W23 -- apt update
+lxc exec COMP2021-W23 -- apt install apache2
 echo "Done."
 
 echo
-echo "Checking Juju status..."
-juju status
+echo "Gathering information..."
 
-exit
+iface=$(lxc exec COMP2021-W23 bash -e "ip route | head -n 1 | awk '{ print $5 }")
+ip=$(lxc exec COMP2021-W23 bash -e "ip addr show dev ${iface} | grep -i 'inet ' | awk '{ print $2 }')")
 
-#while true; do
-#  apacheStatus=$(juju status apache2 | grep "^0" | awk 'NR==5{ print $2 }')
-#  [ $apacheStatus -eq "ready" ] && echo "
+grep "${ip}" /etc/hosts | grep "COMP2101-W23"
+if [ $? ]; then echo "${ip} COMP2101-W23" | sudo tee -a /etc/hosts; fi
 
-iface=$(juju ssh 0 bash -e "ip route | head -n 1 | awk '{ print $5 }")
-ip=$(juju ssh 0 bash -e "ip addr show dev ${iface} | grep -i 'inet ' | awk '{ print $2 }')")
-
-grep "${ip}" /etc/hosts | grep "COMP2101-S22"
-if [ $? ]; then echo "${ip} COMP2101-S22" | sudo tee -a /etc/hosts; fi
-
-curl http://COMP2101-S22
+curl http://COMP2101-W23
