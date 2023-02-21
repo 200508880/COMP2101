d43aa1224436e66929a80f2c4e77c8624779dad5
200508880 <200508880@student.georgianc.on.ca>
2023-02-21 14:56:40 -0500

    Ran more tests before reverting and it's good that I did. Fixed some bad logic, renamed all instances to the correct COMP2101.

--- bash/lab3/lxd.sh
+++ bash/lab3/lxd.sh
@@ -11,29 +11,29 @@
 # Reverting back to lxd itself (which is still only available on 22.04 as a Snap)
 echo "Deploying a webserver container."
 
-#iface=$(ip route | head -n 1 | awk '{ print $5 }')
-#ip=$(ip addr show dev "${iface}" | grep -i "inet " | awk '{ print $2 }')
-
-apt-cache policy curl | grep Installed | grep none > /dev/null
-[ $? ] && echo "Curl not installed. Installing Curl now." && sudo apt install curl && echo
+# Just to keep it straight in my own head, a grep exit code of 0 means matches were found
+# grep exit code of 1 means no matches were found
+# For these tests, any number of matches means no action is required.
+apt-cache policy curl | grep Installed > /dev/null
+[ $? -ne "0" ] && echo "Curl not installed. Installing Curl now." && sudo apt install curl && echo
 
 # Nope, not falling for THAT again.
 #snap list | grep "^juju" > /dev/null
 #[ $? ] && echo "Juju not installed. Installing Juju now." && sudo snap install juju --classic && echo
 
 snap list | grep "^lxd" > /dev/null
-[ $? ] && echo "lxd not installed. Installing lxd now." && sudo snap install lxd --classic && echo
+[ $? -ne "0" ] && echo "lxd not installed. Installing lxd now." && sudo snap install lxd --classic && echo
 
 echo "Initializing lxd container..."
 
 ip link show | grep "lxdbr0" > /dev/null
-[ $? ] && echo "Setting up network bridge..." && lxd init --auto
+[ $? -ne "0" ] && echo "Setting up network bridge..." && lxd init --auto
 
 echo
 echo "Installing Apache2 container..."
-lxc launch ubuntu:20.04 COMP2021-W23
-lxc exec COMP2021-W23 -- apt update
-lxc exec COMP2021-W23 -- apt install -y apache2
+lxc launch ubuntu:20.04 COMP2101-W23
+lxc exec COMP2101-W23 -- apt update > /dev/null
+lxc exec COMP2101-W23 -- apt install -y apache2 > /dev/null
 echo "Done."
 
 echo
@@ -41,10 +41,11 @@ echo "Gathering information..."
 # Some combination of lxc exec and awk is causing issues, process here
 # There's probably a more proper way to do it but my brain is fried so here's this mess:
 
-iproute=$(lxc exec COMP2021-W23 -- ip route | head -n 1)
+iproute=$(lxc exec COMP2101-W23 -- ip route | head -n 1)
 iface=$(echo $iproute | awk '{ print $5 }')
 echo "Container network interface name is ${iface}."
-ipRaw=$(lxc exec COMP2021-W23 --env iface=${iface} -- bash -c "ip addr show dev ${iface} | grep -i 'inet '")
+# Had to look up how to pass variables INTO an lxc. Probably there was a one-line solution for this.
+ipRaw=$(lxc exec COMP2101-W23 --env iface=${iface} -- bash -c "ip addr show dev ${iface} | grep -i 'inet '")
 ip=$(echo $ipRaw | awk '{ print $2 }')
 # Trim the subnet prefix
 ip=${ip:0:-3}
@@ -52,14 +53,18 @@ echo "Container IP is ${ip}."
 
 echo "Entering COMP2101-W23 into VM's hosts file..."
 grep "${ip}" /etc/hosts | grep "COMP2101-W23" > /dev/null
-if [ $? ]; then echo "${ip} COMP2101-W23" | sudo tee -a /etc/hosts; fi
+if [ $? -ne "0" ]; then echo "${ip} COMP2101-W23" | sudo tee -a /etc/hosts; fi
 echo "Done."
 
 echo
 echo "Testing webserver..."
 curl http://COMP2101-W23 -o /tmp/curlout
 curlExitCode=$?
+
+echo
+echo "Webpage output:"
+echo
 head /tmp/curlout
 
 echo
-[ $curlExitCode ] && "Webserver setup successful. Consider exposing it to your local network." || echo "Webserver setup unsuccessful. It worked when I tried it!"
+[ $curlExitCode -eq "0" ] && echo "Webserver setup successful. Consider exposing it to your local network." || echo "Webserver setup unsuccessful. It worked when I tried it!"
