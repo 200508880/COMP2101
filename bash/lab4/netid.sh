609bc75e7fabe3e58aede2cb2a8ccf29e01b9bc3
200508880 <200508880@student.georgianc.on.ca>
2023-03-01 22:29:33 -0500

    Finished netid.sh, took screenshots

--- bash/lab4/netid.sh
+++ bash/lab4/netid.sh
@@ -27,6 +27,62 @@
 # grep is used to filter ip command output so we don't have extra junk in our output
 # stream editing with sed and awk are used to extract only the data we want displayed
 
+interface="enp1s0"
+allInterfaces="no"
+
+while [ $# -gt 0 ]; do
+  # I was going to do this as a series of ifs to be different from the powerpoint but cases are just too good
+  case "$1" in
+    -h | --help )
+      echo "Usage: netid.sh [options] [interface name]"
+      echo "Options:"
+      echo "  -h or --help: show this information"
+      echo "  -v or --verbose: print out extra information on execution"
+      echo "  -l or --list: list network interfaces by name"
+      echo "  -a or --all: gather information on every interface"
+      echo "  string: specify a network interface by name to inspect"
+      echo
+      echo "Example:"
+      echo "  netid.sh -v eno1"
+      echo
+      exit
+      ;;
+    -l | --list )
+      # lot of trial and error here
+      # fix the veth @if4 business since that's not real
+      # in fact, I hope you don't mind if I nuke virtual ethernet entirely, it causes very headache
+      menu=($(ip link show | grep -v "^ " | grep -v "veth" | awk '{ print substr($2,0,length($2)-1) }' | awk -F'@' '{ print $1 }'))
+      menuString=""
+      #for (( i=0; i<${#menu}; i++ )); do; menuString="$menuString ${!menu[i]} ${menu[i]}"; done
+      i=0
+      # I feel like iterating through a bash array should work without having to specify the [@]
+      for menuItem in ${menu[@]}; do
+        menuString="${menuString}$i $menuItem "
+        ((i=i+1))
+      done
+      selected=$(dialog --menu --stdout "Please select a network interface" 14 40 8 ${menuString})
+      interface="${menu[$selected]}"
+      ;;
+    -v | --verbose )
+      verbose="yes"
+      ;;
+    -a | --all )
+      allInterfaces="yes"
+      ;;
+    * )
+      # attempting to adhere to the specific wording "if the user includes one and only one string..."
+      if [ $# -eq "1" ]; then
+        interface="$1"
+      else
+        # This being after all the actual flags means it should only be reached when there's only supposed to be one thing left anyway
+        echo "Please only provide one interface name."
+        exit
+      fi
+      ;;
+  esac
+  shift
+done
+
 #####
 # Once per host report
 #####
@@ -72,23 +128,26 @@ EOF
 #####
 
 # define the interface being summarized
-interface="eno1"
-[ "$verbose" = "yes" ] && echo "Reporting on interface(s): $interface"
+# moving up so it can be overwritten by argument or menu option
+#interface="enp1s0"
 
-[ "$verbose" = "yes" ] && echo "Getting IPV4 address and name for interface $interface"
-# Find an address and hostname for the interface being summarized
-# we are assuming there is only one IPV4 address assigned to this interface
-ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
-ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
+getInterface () {
+  [ "$verbose" = "yes" ] && echo "Reporting on all interface(s): $interface"
+  [ "$verbose" = "yes" ] && echo "Getting IPV4 address and name for interface $interface"
+  # Find an address and hostname for the interface being summarized
+  # we are assuming there is only one IPV4 address assigned to this interface
+  ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
+  ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
 
-[ "$verbose" = "yes" ] && echo "Getting IPV4 network block info and name for interface $interface"
-# Identify the network number for this interface and its name if it has one
-# Some organizations have enough networks that it makes sense to name them just like how we name hosts
-# To ensure your network numbers have names, add them to your /etc/networks file, one network to a line, as   networkname networknumber
-#   e.g. grep -q mynetworknumber /etc/networks || (echo 'mynetworkname mynetworknumber' |sudo tee -a /etc/networks)
-network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
-network_number=$(cut -d / -f 1 <<<"$network_address")
-network_name=$(getent networks $network_number|awk '{print $1}')
+  [ "$verbose" = "yes" ] && echo "Getting IPV4 network block info and name for interface $interface"
+  # Identify the network number for this interface and its name if it has one
+  # Some organizations have enough networks that it makes sense to name them just like how we name hosts
+  # To ensure your network numbers have names, add them to your /etc/networks file, one network to a line, as   networkname networknumber
+  #   e.g. grep -q mynetworknumber /etc/networks || (echo 'mynetworkname mynetworknumber' |sudo tee -a /etc/networks)
+  network_address=$(ip route list dev $interface scope link | grep "${ipv4_address}" | cut -d ' ' -f 1)
+  [ "$interface" = "lo" ] && network_address="127.0.0.0"
+  network_number=$(cut -d / -f 1 <<<"$network_address")
+  network_name=$(getent networks $network_number|awk '{print $1}')
 
 cat <<EOF
 
@@ -100,6 +159,26 @@ Network Address : $network_address
 Network Name    : $network_name
 
 EOF
+}
+
+
+# moving this below function definition:
+# turning this into a regular condition so I can make it more complex without getting a headache
+
+if [ "$allInterfaces" = "yes" ]; then
+    # I should have just done this once outside of the case so I wouldn't have to duplicate it:
+    # ah yes, and now that I have to modify it to string the @if4 business out, I have to fix it in two places
+    interfaces=$(ip link show | grep -v "^ " | grep -v "veth" | awk '{ print substr($2,0,length($2)-1) }' | awk -F'@' '{ print $1 }')
+    # This seems cheeky... can I do it?
+    # seems so
+    for interface in $interfaces; do
+      getInterface
+    done
+  else
+    echo "Reporting on interface(s): $interface"
+    getInterface
+  fi
+
 #####
 # End of per-interface report
 #####
