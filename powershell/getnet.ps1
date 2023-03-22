e9d23a2dc1b5521c05c429d7741e7d2c7940bcbf
200508880 <200508880@student.georgianc.on.ca>
2023-03-22 08:33:55 -0700

    Refined script. Iterates through interfaces and generates a new object.

--- powershell/getnet.ps1
+++ powershell/getnet.ps1
@@ -1,1 +1,7 @@
-get-ciminstance Win32_NetworkAdapterConfiguration | ? ipenabled -eq $true | select Index, Description, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder | format-table
+$getnetraw = get-ciminstance Win32_NetworkAdapterConfiguration | ? ipenabled -eq $true | select Index, Description, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
+
+foreach ($gnr in $getnetraw){
+	$getnet += new-object -typename psobject -property @{Index=$gnr.Index; Description=$gnr.Description; IPv4Address=$gnr.IPAddress[0]; IPv6Address=$gnr.IPAddress[1]; IPSubnet=$gnr.IPSubnet[0]; DNSDomain=$gnr.DNSDomain; DNSServer=$gnr.DNSServerSearchOrder[0] }
+}
+
+$getnet | select Index, Description, IPv4Address, IPSubnet, DNSDomain, DNSServer, IPv6Address | format-list
