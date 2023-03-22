fc6475b41d216b6d17d1be8041c24da4dc521513
200508880 <200508880@student.georgianc.on.ca>
2023-03-22 08:41:41 -0700

    Updated to use inline hash notation to specify new elements as per lesson 3. This is way better.

--- powershell/getnet.ps1
+++ powershell/getnet.ps1
@@ -1,7 +1,12 @@
-$getnetraw = get-ciminstance Win32_NetworkAdapterConfiguration | ? ipenabled -eq $true | select Index, Description, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
+#$getnetraw = get-ciminstance Win32_NetworkAdapterConfiguration | ? ipenabled -eq $true | select Index, Description, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
 
-foreach ($gnr in $getnetraw){
-	$getnet += new-object -typename psobject -property @{Index=$gnr.Index; Description=$gnr.Description; IPv4Address=$gnr.IPAddress[0]; IPv6Address=$gnr.IPAddress[1]; IPSubnet=$gnr.IPSubnet[0]; DNSDomain=$gnr.DNSDomain; DNSServer=$gnr.DNSServerSearchOrder[0] }
-}
+#foreach ($gnr in $getnetraw){
+#	$getnet += new-object -typename psobject -property @{Index=$gnr.Index; Description=$gnr.Description; IPv4Address=$gnr.IPAddress[0]; IPv6Address=$gnr.IPAddress[1]; IPSubnet=$gnr.IPSubnet[0]; DNSDomain=$gnr.DNSDomain; DNSServer=$gnr.DNSServerSearchOrder[0] }
+#}
 
-$getnet | select Index, Description, IPv4Address, IPSubnet, DNSDomain, DNSServer, IPv6Address | format-list
+#$getnet | select Index, Description, IPv4Address, IPSubnet, DNSDomain, DNSServer, IPv6Address | format-list
+
+#$getnetraw | Select-Object 
+
+# With hash notation:
+get-ciminstance Win32_NetworkAdapterConfiguration | ? ipenabled -eq $true | select Description, Index, @{n="IPv4Address"; e={$_.IPAddress[0]}}, @{n="IPv6Address"; e={$_.IPAddress[1]}}, @{n="Subnet"; e={$_.IPSubnet[0]}}, DNSDomain, @{n="DNSServer"; e={$_.DNSServerSearchOrder[0]}}
